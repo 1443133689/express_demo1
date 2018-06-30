75c61b6ec06729c04d9a3622aa7210d170758957
phx <1443133689@qq.com>
2018-06-30 09:31:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// 系统模块
+const express = require('express');
+
+const app = express();
+
+// 第三方模块
+const hbs = require('express3-handlebars');
+
+const PORT = process.env.PORT || 3000;
+// 视图引擎
+app.engine('hbs', hbs({ // 注册模板引擎处理后缀名为 hbs 的文件
+	// 默认模板引擎的文件夹为 views即 layoutDir:'views'
+	defaultLayout:'main', // 基础布局模板为main.hbs,取消在reder时传入{layout:false}
+	extname:'.hbs' // 指定模板后缀，默认为.handlebars
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
+app.use((req, res) => {
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - not found');
+});
+
+app.use((err, req, res, next) => {
+	console.log(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - server error');
+});
+
+app.listen(PORT);
+
+console.log('服务运行在'+PORT);
